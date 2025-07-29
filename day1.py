6fc130620cdf4e5d61e068846ddf286d2e547bd5
srinivas-palsa <palsasrinivas03@gmail.com>
2025-07-29 22:51:18 +0530

    Day1 Python Practice

---
+++ day1.py
@@ -0,0 +1,29 @@
+# # Python program to demonstrate basic input/output
+
+# # name = input("Enter your name: ")
+# # age = int(input("Enter your age: "))
+# # city = input("Enter your city: ")
+# # print(f"Hello {name}, you are {age} years old and live in {city}.")
+
+
+
+# # Python program to demonstrate string operations
+
+### Reversing a string
+# x= "srinivas"
+# y=x[::-1]
+# print(y)
+
+
+# str="Srinivas".lower() #.lower is used to make whole word into small letters
+# print(str[0]==str[-1])
+
+
+
+#### STRING BUILT IN FUNCTIONS
+
+name ="srinivas palsa"
+print(name.endswith("sa"))
+print(name.capitalize())
+print(name.replace("s","v"))
+print(name.find("a")) # shows first 'a' position from the string
