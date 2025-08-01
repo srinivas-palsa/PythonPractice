c47fab86ee40be47aa664f06e13313c520ec7bb3
srinivas-palsa <palsasrinivas03@gmail.com>
2025-08-01 14:14:56 +0530

    Day4 Python Practice

--- Day4.py
+++ Day4.py
@@ -23,16 +23,19 @@
 #     print()
 
 
+
 # pattern 3
-#
+
 # n=int(input("Enter num of rows: "))
 # for i in range(1,n+1):
 #     for j in range(1,i+1):
 #         print(j, end=" ")
 #     print()
 
+
+
 # pattern 4
-#
+
 # n=int(input("Enter num of rows: "))
 # for i in range(n,0,-1):
 #     for j in range(1,i+1):
